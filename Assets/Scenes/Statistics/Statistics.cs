acbb602745fb044d642c819275f1ee8253acf07c
Rafał Jodłowski <rafaljodlowski24@gmail.com>
2024-02-24 01:26:18 +0100

    stats

---
+++ Assets/Scenes/Statistics/Statistics.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Statistics
+{
+    // Player variables
+    private float playerDamage { get; set; }
+    private float playerHealth { get; set; }
+    private float playerSpeed { get; set; }
+    private float playerStamina { get; set; }
+    private float playerMiningSpeed { get; set; }
+
+    // Enemy variables
+    private float enemyHealth { get; set; }
+    private float enemyDamage { get; set; }
+    private float enemySpeed { get; set; }
+}
