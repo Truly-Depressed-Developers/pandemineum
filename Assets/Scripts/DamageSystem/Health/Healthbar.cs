303f778ae40dae2f4a4b0a99feba2a916728be6b
Spookyless <spookyless.info@gmail.com>
2024-02-24 10:23:47 +0100

    Healthbary są

---
+++ Assets/Scripts/DamageSystem/Health/Healthbar.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace DamageSystem.Health {
+  public class Healthbar : MonoBehaviour {
+    [SerializeField] private Slider slider;
+    [SerializeField] private Image fill;
+    [SerializeField] private Image border;
+
+    public void SetHealth(float health) {
+      slider.value = health;
+
+      bool isHidden = Mathf.Abs(slider.value - slider.maxValue) < Mathf.Epsilon;
+      fill.enabled = !isHidden;
+      border.enabled = !isHidden;
+    }
+
+    public void SetMaxHealth(float health) {
+      slider.maxValue = health;
+      
+      SetHealth(health);
+    }
+  }
+}
