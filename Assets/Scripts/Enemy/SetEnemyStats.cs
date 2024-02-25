dcf39ad6d740dc87ef7c6fcb080c676e51a28d7e
Spookyless <spookyless.info@gmail.com>
2024-02-25 12:30:10 +0100

    fix

--- Assets/Scripts/Enemy/SetEnemyStats.cs
+++ Assets/Scripts/Enemy/SetEnemyStats.cs
@@ -2,19 +2,17 @@
 using Statistics;
 using UnityEngine;
 
-namespace Enemy {
-  [DefaultExecutionOrder(-999)]
-  public class SetEnemyStats : MonoBehaviour {
-    [SerializeField] private Receiver receiver;
-    [SerializeField] private Kobold kobold;
-    [SerializeField] private CollisionDamageDealer collisionDamageDealer;
+[DefaultExecutionOrder(-999)]
+public class SetEnemyStats : MonoBehaviour {
+  [SerializeField] private Receiver receiver;
+  [SerializeField] private Enemy kobold;
+  [SerializeField] private CollisionDamageDealer collisionDamageDealer;
 
-    private void Start() {
-      receiver.baseMaxHp *= StatisticsRepo.I.EnemyHealthMaxMul;
-      receiver.baseArmor += StatisticsRepo.I.EnemyArmorAdd;
-      kobold.minJumpDistance *= StatisticsRepo.I.EnemyAttackRangeMul;
-      kobold.movementSpeed *= StatisticsRepo.I.EnemySpeedMul;
-      collisionDamageDealer.damage += StatisticsRepo.I.EnemyDamageAdd;
-    }
+  private void Start() {
+    receiver.baseMaxHp *= StatisticsRepo.I.EnemyHealthMaxMul;
+    receiver.baseArmor += StatisticsRepo.I.EnemyArmorAdd;
+    kobold.minJumpDistance *= StatisticsRepo.I.EnemyAttackRangeMul;
+    kobold.movementSpeed *= StatisticsRepo.I.EnemySpeedMul;
+    collisionDamageDealer.damage += StatisticsRepo.I.EnemyDamageAdd;
   }
 }
