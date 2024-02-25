4f00bcd972b03a9ff7f7b694fcf2a890cfd70ef4
Xenoneqq <realfatboimlg@gmail.com>
2024-02-25 10:16:43 +0100

    kolejnosc wyswietlania dodana

---
+++ Assets/Scripts/Enviroment/OrderRenderer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utils {
+  public class OrderRenderer : MonoBehaviour {
+    [SerializeField] private bool realtime_updates = true;
+    [SerializeField] private Transform ordering_anchor;
+    [Space(5)]
+    [SerializeField] private SpriteRenderer[] sprite_rend_array;
+    [SerializeField] private int[] layer_offset;
+    private int base_y , i;
+
+    private void Start() {
+      calculate_layers();
+    }
+
+    private void Update() {
+      if(realtime_updates)
+        calculate_layers();
+    }
+
+    private void calculate_layers() {
+      if (sprite_rend_array == null || sprite_rend_array.Length == 0) return;
+      base_y = -(int)(ordering_anchor.transform.position.y * 100);
+      for(i = 0; i < sprite_rend_array.Length; i++) {
+        sprite_rend_array[i].sortingOrder = base_y + layer_offset[i];
+      }
+    }
+  }
+}
