449e523bc58270c0ef7d85735a4b23dfdb080d5c
rjodlowski <rafaljodlowski24@gmail.com>
2024-02-25 01:16:19 +0100

    we got tha stat update workin'

--- Assets/Scenes/Cards/CardGenerator.cs
+++ Assets/Scenes/Cards/CardGenerator.cs
@@ -5,60 +5,49 @@ using UnityEngine.UI;
 using TMPro;
 using System;
 
-public class CardPrefabGenerator : MonoBehaviour
-{
-    void Start()
-    {
-        this.GenerateCardPrefab();
-    }
-    public void GenerateCardPrefab()
-    {
-        // Get the child and draw stats
-        for (int i = 0; i < this.transform.childCount; i++)
-        {
-            // Get the card
-            var card = this.transform.GetChild(i);
-            
-            // Display each statistic
-            for (int j = 0; j < card.childCount; j++)
-            {
-                // Draw statistics for it
-                var cardStatisticsScriptableObjects = ScriptableObject.CreateInstance<CardStatistics>();
-                var displayStatistics = card.transform.GetChild(j);
+public class CardPrefabGenerator : MonoBehaviour {
+  void Start() {
+    this.GenerateCardPrefab();
+  }
+  public void GenerateCardPrefab() {
+    // Get the child and draw stats
+    for (int i = 0; i < this.transform.childCount; i++) {
+      // Get the card
+      var card = this.transform.GetChild(i);
+
+      // Display each statistic
+      for (int j = 0; j < card.childCount; j++) {
+        // Draw statistics for it
+        var cardStatisticsScriptableObjects = ScriptableObject.CreateInstance<CardStatistics>();
+        var displayStatistics = card.transform.GetChild(j);
+        var buff = cardStatisticsScriptableObjects.buffType;
+
+        // Pass them onto realization
         var displayScript = displayStatistics.parent.GetComponent<CardSelectionHandler>();
         displayScript.statistics = cardStatisticsScriptableObjects;
-                var buff = cardStatisticsScriptableObjects.buffType;
-                // Fetch the script with text fields to set their values
-                card5 card5 = displayStatistics.GetComponent<card5>();
-                
 
-                // Fill out text fields' values
-                string sign = "";
-                if (cardStatisticsScriptableObjects.value>0)
-                {
-                    sign = "+";
-                }
-                card5.value.text = sign + cardStatisticsScriptableObjects.value.ToString();
+        // Fetch the script with text fields to set their values
+        card5 card5 = displayStatistics.GetComponent<card5>();
 
-                card5.entityType.text = cardStatisticsScriptableObjects.entityType.ToString();
-                if (cardStatisticsScriptableObjects.entityType == EntityType.Player)
-                {
-                    card5.chosenStatistic.text = cardStatisticsScriptableObjects.playerStatistic.ToString();
-                }
-                card5.chosenStatistic.text = cardStatisticsScriptableObjects.enemyStatistic.ToString();
-                if (buff == BuffType.Buff)
-                {
-                    card5.value.color = Color.green;
-                    card5.entityType.color = Color.green;
-                    card5.chosenStatistic.color = Color.green;  
-                }
-                else
-                {
-                    card5.value.color = Color.red;
-                    card5.entityType.color = Color.red;
-                    card5.chosenStatistic.color = Color.red;
-                }
-            }
+        // Fill out text fields' values
+        string sign = cardStatisticsScriptableObjects.value > 0 ? "+" : "";
+        card5.value.text = sign + cardStatisticsScriptableObjects.value.ToString();
+
+        card5.entityType.text = cardStatisticsScriptableObjects.entityType.ToString();
+        if (cardStatisticsScriptableObjects.entityType == EntityType.Player) {
+          card5.chosenStatistic.text = cardStatisticsScriptableObjects.playerStatistic.ToString();
         }
+        card5.chosenStatistic.text = cardStatisticsScriptableObjects.enemyStatistic.ToString();
+        if (buff == BuffType.Buff) {
+          card5.value.color = Color.green;
+          card5.entityType.color = Color.green;
+          card5.chosenStatistic.color = Color.green;
+        } else {
+          card5.value.color = Color.red;
+          card5.entityType.color = Color.red;
+          card5.chosenStatistic.color = Color.red;
+        }
+      }
     }
+  }
 }
--- Assets/Scripts/Statistics/CardSelectionHandler.cs
+++ Assets/Scripts/Statistics/CardSelectionHandler.cs
@@ -2,83 +2,161 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class CardSelectionHandler : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, ISelectHandler, IDeselectHandler
-{
-    [SerializeField] private float verticalMoveAmount = 30f;
-    [SerializeField] private float moveTime = 0.1f;
-    [Range(0, 2f), SerializeField] private float scaleAmount = 1.05f;
+public class CardSelectionHandler : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, ISelectHandler, IDeselectHandler {
+  [SerializeField] private float verticalMoveAmount = 30f;
+  [SerializeField] private float moveTime = 0.1f;
+  [Range(0, 2f), SerializeField] private float scaleAmount = 1.05f;
 
-    public CardStatistics statistics = null;
+  public CardStatistics statistics = null;
 
-    private Vector3 startPos;
-    private Vector3 startScale;
+  private Vector3 startPos;
+  private Vector3 startScale;
 
-    void Start()
-    {
-        this.startPos = transform.position;
-        this.startScale = transform.localScale;
+  void Start() {
+    this.startPos = transform.position;
+    this.startScale = transform.localScale;
+  }
+
+  private IEnumerator animateCards(bool startingAnimation) {
+    Vector3 endPosition;
+    Vector3 endScale;
+
+    float elapsedTime = 0f;
+    while (elapsedTime < this.moveTime) {
+      // Increment timer
+      elapsedTime += Time.deltaTime;
+
+      // Assess move direction
+      if (startingAnimation) {
+        endPosition = this.startPos + new Vector3(0f, this.verticalMoveAmount, 0f);
+        endScale = this.startScale * this.scaleAmount;
+      } else {
+        endPosition = this.startPos;
+        endScale = this.startScale;
+      }
+
+      // Calculate the step
+      Vector3 lerpedPosition = Vector3.Lerp(transform.position, endPosition, (elapsedTime / moveTime));
+      Vector3 lerpedScale = Vector3.Lerp(transform.localScale, endScale, (elapsedTime / moveTime));
+
+      // Apply the changes
+      transform.position = lerpedPosition;
+      transform.localScale = lerpedScale;
+
+      // Animation stuff - await next frame kind of
+      yield return null;
     }
+  }
 
-    private IEnumerator animateCards(bool startingAnimation)
-    {
-        Vector3 endPosition;
-        Vector3 endScale;
+  public void OnPointerEnter(PointerEventData eventData) {
+    // Select the card
+    eventData.selectedObject = gameObject;
+  }
 
-        float elapsedTime = 0f;
-        while (elapsedTime < this.moveTime)
-        {
-            // Increment timer
-            elapsedTime += Time.deltaTime;
+  public void OnPointerExit(PointerEventData eventData) {
+    // Deselect the card
+    eventData.selectedObject = null;
+  }
 
-            // Assess move direction
-            if(startingAnimation)
-            {
-                endPosition = this.startPos + new Vector3(0f, this.verticalMoveAmount, 0f);
-                endScale = this.startScale * this.scaleAmount;
-            } else
-            {
-                endPosition = this.startPos;
-                endScale = this.startScale;
-            }
+  public void OnSelect(BaseEventData eventData) {
+    StartCoroutine(this.animateCards(true));
+  }
 
-            // Calculate the step
-            Vector3 lerpedPosition = Vector3.Lerp(transform.position, endPosition, (elapsedTime / moveTime));
-            Vector3 lerpedScale = Vector3.Lerp(transform.localScale, endScale, (elapsedTime / moveTime));
+  public void OnDeselect(BaseEventData eventData) {
+    StartCoroutine(this.animateCards(false));
+  }
 
-            // Apply the changes
-            transform.position = lerpedPosition;
-            transform.localScale = lerpedScale;
+  public void onClick() {
+    //Debug.Log("clicked");
+    //Debug.Log(this.statistics.value);
+    this.updateStats(this.statistics);
+  }
 
-            // Animation stuff - await next frame kind of
-            yield return null;
+  public void updateStats(CardStatistics cardStatisticsScriptableObjects) {
+    switch (cardStatisticsScriptableObjects.entityType) {
+      case EntityType.Player: {
+          switch (cardStatisticsScriptableObjects.playerStatistic) {
+            case PlayerStatistics.Health: {
+                StatisticsRepo.I.playerHealthMaxMul *= cardStatisticsScriptableObjects.value;
+                break;
+              }
+            case PlayerStatistics.Armor: {
+                StatisticsRepo.I.playerArmorAdd += cardStatisticsScriptableObjects.value;
+                break;
+              }
+            case PlayerStatistics.Damage: {
+                StatisticsRepo.I.playerDamageAdd += cardStatisticsScriptableObjects.value;
+                break;
+              }
+            case PlayerStatistics.Speed: {
+                StatisticsRepo.I.playerSpeedMul *= cardStatisticsScriptableObjects.value;
+                break;
+              }
+            case PlayerStatistics.ReloadSpeed: {
+                StatisticsRepo.I.playerSpeedMul *= cardStatisticsScriptableObjects.value;
+                break;
+              }
+            case PlayerStatistics.ShotRange: {
+                StatisticsRepo.I.playerShotRangeMul *= cardStatisticsScriptableObjects.value;
+                break;
+              }
+            case PlayerStatistics.SightRange: {
+                StatisticsRepo.I.playerSightRangeMul *= cardStatisticsScriptableObjects.value;
+                break;
+              }
+            case PlayerStatistics.Luck: {
+                StatisticsRepo.I.playerLuckMul *= cardStatisticsScriptableObjects.value;
+                break;
+              }
+            case PlayerStatistics.CobaltPickRate: {
+                StatisticsRepo.I.playerCobaltPickRateMul *= cardStatisticsScriptableObjects.value;
+                break;
+              }
+            default: {
+                Debug.LogWarning("Unknown Player property type");
+                break;
+              }
+          }
+          break;
+        }
+      case EntityType.Enemy: {
+          switch (cardStatisticsScriptableObjects.enemyStatistic) {
+            case EnemyStatistics.Health: {
+                StatisticsRepo.I.enemyHealthMaxMul *= cardStatisticsScriptableObjects.value;
+                break;
+              }
+            case EnemyStatistics.Armor: {
+                StatisticsRepo.I.enemyArmorAdd += cardStatisticsScriptableObjects.value;
+                break;
+              }
+            case EnemyStatistics.Damage: {
+                StatisticsRepo.I.enemyDamageAdd += cardStatisticsScriptableObjects.value;
+                break;
+              }
+            case EnemyStatistics.Speed: {
+                StatisticsRepo.I.enemySpeedMul *= cardStatisticsScriptableObjects.value;
+                break;
+              }
+            case EnemyStatistics.ShotRange: {
+                StatisticsRepo.I.playerShotRangeMul *= cardStatisticsScriptableObjects.value;
+                break;
+              }
+            case EnemyStatistics.DropRate: {
+                StatisticsRepo.I.enemyDropRateMul *= cardStatisticsScriptableObjects.value;
+                break;
+              }
+            default: {
+                Debug.LogWarning("Unknown enemy property type");
+                break;
+              }
+          }
+          break;
+        }
+      default: {
+          Debug.LogWarning("Unknown entity type");
+          break;
         }
     }
-
-    public void OnPointerEnter(PointerEventData eventData)
-    {
-        // Select the card
-        eventData.selectedObject = gameObject;
-    }
-
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        // Deselect the card
-        eventData.selectedObject = null;
-    }
-
-    public void OnSelect(BaseEventData eventData)
-    {
-        StartCoroutine(this.animateCards(true));
-    }
-
-    public void OnDeselect(BaseEventData eventData)
-    {
-        StartCoroutine(this.animateCards(false));   
-    }
-
-    public void onClick() {
-        Debug.Log("clicked");
-        Debug.Log(this.statistics.value);
-    }
+  }
 }
 
